c0ae1c9185a9f262637fd184769e1f4d995dda80
akaboshinit <akaboshinit@gmail.com>
2020-08-11 15:03:35 +0900

    remove

--- out/_next/static/F6y2GdZlfQ7NZmPS_O8Ub/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-d79673177a885914eae0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ff3673b4e910f9435a1e.js"],"/about":["static\u002Fchunks\u002Fpages\u002Fabout-7c2325edb232c609cb24.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- out/_next/static/gmRXSYRaiN0o0VSL2N_Ih/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-d79673177a885914eae0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ff3673b4e910f9435a1e.js"],"/about":["static\u002Fchunks\u002Fpages\u002Fabout-973513115b5bd6ba897e.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
