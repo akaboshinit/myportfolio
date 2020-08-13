278ad8d7c88ee8a290250d03f58dd23ec03379de
akaboshinit <akaboshinit@users.noreply.github.com>
2020-08-13 09:24:56 +0000

    deploy: f3e12749f1efe668cca3876a97f9b9b72df4e67f

---
+++ _next/static/jqWHYO3mBbYtS2GCglzvF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-fe93651741a75350d9b1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"],"/about":["static\u002Fchunks\u002Fpages\u002Fabout-75189e2b7d0793d20e42.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
