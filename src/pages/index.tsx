2fdcfe62ff50aed8a66e2e150af879f6521f0dc4
akaboshinit <akaboshinit@gmail.com>
2020-08-17 15:58:36 +0900

    index redirect update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
+const dir = String(process.env.BACKEND_URL);
 import Router from 'next/router'
 
 const Index = (props : any) => {
@@ -8,7 +9,7 @@ const Index = (props : any) => {
   useEffect(() => { //redirect
     const {pathname} = Router
     if(pathname == '/' ){
-        Router.push('/category/about')
+        Router.push(dir+'/category/about')
     }
   });
   return (
