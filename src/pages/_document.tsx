bae067ddce130492938fea75225e65bd3c20163b
akaboshinit <akaboshinit@gmail.com>
2020-08-17 11:12:29 +0900

    styled tikatika test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,13 +7,36 @@ type Props = {
 };
 
 export default class MyDocument extends Document<Props> {
-    static getInitialProps({renderPage}:{renderPage:any}) {
-        const sheet = new ServerStyleSheet();
-        const page = renderPage((App:any) => (props:any) =>
-            sheet.collectStyles(<App {...props} />),
-        );
-        const styleTags = sheet.getStyleElement();
-        return {...page, styleTags};
+    // static getInitialProps({renderPage}:{renderPage:any}) {
+    //     const sheet = new ServerStyleSheet();
+    //     const page = renderPage((App:any) => (props:any) =>
+    //         sheet.collectStyles(<App {...props} />),
+    //     );
+    //     const styleTags = sheet.getStyleElement();
+    //     return {...page, styleTags};
+    // }
+    static async getInitialProps(ctx:any) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                enhanceApp: (App:any) => (props:any) =>
+                    sheet.collectStyles(<App {...props} />),
+                })
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: (
+                <>
+                    {initialProps.styles}
+                    {sheet.getStyleElement()}
+                </>
+                ),
+            }
+        } finally {
+            sheet.seal()
+        }
     }
 
     render() {
@@ -27,7 +50,7 @@ export default class MyDocument extends Document<Props> {
                     <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css"></link>
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed:700"></link>
                     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"></link>
-                    {this.props.styleTags}
+                    {/* {this.props.styleTags} */}
                 </Head>
             <body>
                 <Main />
